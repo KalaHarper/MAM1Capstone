4feb0cf17cecffd596c607ed7c67ff3f879e4d7d
CodeCowboy <michael.klingenberg.cpt@gmail.com>
2022-04-12 09:58:04 -0500

    Slight readability improvements

--- capstone/src/main/java/com/techelevator/Inventory.java
+++ capstone/src/main/java/com/techelevator/Inventory.java
@@ -8,47 +8,47 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Inventory {
+    private static final String MAX_STOCK = "|5"; //Created constant
     private static final String DEFAULT_LOADOUT = "capstone/vendingmachine.csv";
     private static String[] inventoryVariables = new String[5];
-   //TODO: refactor list into hashmap
     private static List<String[]> inventoryList = new ArrayList<>();
     private static File manifest = new File(DEFAULT_LOADOUT);
-    // private static HashMap<String, String> slotIndicatorHashMap = new HashMap<>();
-    private static HashMap<String, String[]> testerMap = new HashMap<>();
+    private static HashMap<String, String[]> productInfoBySlot = new HashMap<>(); //Removed old List
 
-    public Inventory() {
-//        List<String[]> inventoryList = Inventory.inventoryList;
+    public Inventory() {} //Removed List that was declared in here
 
+    //Made a method to display contents, should improve readability (Anything with an Inventory
+    //instance can now simply call "inventory.displayContents")
+    public void displayContents(){
+        for (String[] item : productInfoBySlot.values()){
+            for(String detail : item){
+                System.out.print(detail+" ");
+            }
+            System.out.println();
+        }
     }
 
     public void loadInventory() {
         try (Scanner manifestReader = new Scanner(manifest)) {
             while (manifestReader.hasNextLine()) {
-                String lineReader = manifestReader.nextLine() + "|5";
+                String lineReader = manifestReader.nextLine() + MAX_STOCK; //Called constant
                 inventoryVariables = lineReader.split("\\|");
-                testerMap.put(inventoryVariables[0], inventoryVariables);
-                //slotIndicatorHashMap.put(inventoryVariables[0], inventoryVariables[1]);
-                inventoryList.add(inventoryVariables);
+                productInfoBySlot.put(inventoryVariables[0], inventoryVariables);
             }
         } catch (FileNotFoundException e) {
-            System.err.println("No file found at that location");
-
+            System.err.println("Inventory file was not loaded properly. Please check file and run again.");
+            System.exit(1); //Changed this (For no real reason)
         }
     }
 
-    //    public static HashMap<String, String> getSlotIndicatorHashMap() {
-//        return slotIndicatorHashMap;
-//    }
-    public static HashMap<String, String[]> getTesterMap() {
-        return testerMap;
-    }
+    public static HashMap<String, String[]> getProductInfoBySlotMap() {
+        return productInfoBySlot;
+    } //Got rid of getList method, still thinking about if we need this one^^^
 
-    public static List<String[]> getInventoryList() {
-        return inventoryList;
-    }
 
-    public static String selectingElementFromTester(String key, int index) {
-        String[] placeholder = testerMap.get(key);
+    //Changed method name for readability vvv
+    public static String getProductValue(String key, int index) {
+        String[] placeholder = productInfoBySlot.get(key);
         return placeholder[index];
     }
 }
--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -2,7 +2,6 @@ package com.techelevator;
 
 import com.techelevator.view.Menu;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class VendingMachineCLI {
@@ -11,16 +10,15 @@ public class VendingMachineCLI {
 	private Menu menu;
 	private static CashBank cashBank;
 	private static Inventory inventory = new Inventory();
-	private final int SLOT_INDICATOR = 0;
-	private final int PRODUCT_NAME = 1;
-	private final int PRICE = 2;
-	private final int PRODUCT_TYPE = 3;
-	private final int AMOUNT_IN_STOCK = 4;
-
+	private static final int SLOT_INDICATOR = 0;
+	private static final int PRODUCT_NAME = 1;
+	private static final int PRICE = 2;
+	private static final int PRODUCT_TYPE = 3;
+	private static final int AMOUNT_IN_STOCK = 4;
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
-	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT };
+	private static final String[] MAIN_MENU_OPTIONS = {MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT};
 	private static final String PURCHASE_MENU_FEED_MONEY = "Feed Money";
 	private static final String PURCHASE_MENU_SELECT_PRODUCT = "Purchase Product";
 	private static final String PURCHASE_MENU_FINISH_TRANSACTION = "Finish Transaction";
@@ -39,56 +37,58 @@ public class VendingMachineCLI {
 
 	public void run() {
 
-
 		inventory.loadInventory();
+
 		while (true) {
+
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-				// display vending machine items
-				displayMenuOptions();
+
+				inventory.displayContents();//implemented new inventory method
+
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-				// do purchase
-				while(true){
+
+				while (true) {
 
 					String purchasingDecision = (String) menu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
-//TODO Start from here next session
-					if (purchasingDecision.equals(PURCHASE_MENU_FEED_MONEY)){
-						while(true){
+					if (purchasingDecision.equals(PURCHASE_MENU_FEED_MONEY)) {
+
+						while (true) {
 
 							String billEntered = (String) menu.getChoiceFromOptions(BILL_DENOMINATIONS);
 
-							if (billEntered.equals(ONE_DOLLAR)){
+							if (billEntered.equals(ONE_DOLLAR)) {
 								cashBank.addMoney(1.00);
-							}else if (billEntered.equals(TWO_DOLLAR)){
+							} else if (billEntered.equals(TWO_DOLLAR)) {
 								cashBank.addMoney(2.00);
-							}else if (billEntered.equals(FIVE_DOLLAR)){
+							} else if (billEntered.equals(FIVE_DOLLAR)) {
 								cashBank.addMoney(5.00);
-							}else if (billEntered.equals(TEN_DOLLAR)){
+							} else if (billEntered.equals(TEN_DOLLAR)) {
 								cashBank.addMoney(10.00);
-							} else if (billEntered.equals(I_HAVE_NO_MONEY)){
+							} else if (billEntered.equals(I_HAVE_NO_MONEY)) {
 								System.out.println("Goodbye");
 								break;
 							}
-							System.out.println("Current Money Provided: \n"+"$"+cashBank.getMoneyProvided());
+							System.out.println("Current Money Provided: \n" + "$" + cashBank.getMoneyProvided());
 						}
-					}else if (purchasingDecision.equals(PURCHASE_MENU_SELECT_PRODUCT)){
-							displayMenuOptions();
-							System.out.println("Please select item. (Example: select A1 for Potato Crisps) ");
-							String input = userInput.nextLine();
-							//if(inventory.getSlotIndicatorHashMap().containsKey(input.toUpperCase())){
-							if(inventory.getTesterMap().containsKey(input.toUpperCase())){
-								System.out.println("This bit still works");
-								//System.out.println(inventory.getTesterMap().get());
-							}else
-								System.out.println("This item does not exist. Please try again.");
+					} else if (purchasingDecision.equals(PURCHASE_MENU_SELECT_PRODUCT)) {
+						inventory.displayContents();//changed method from vendingmachine display method to inventory method
+						System.out.println("Please select item. (Example: select A1 for Potato Crisps) ");
+						String input = userInput.nextLine();
+						//if(inventory.getSlotIndicatorHashMap().containsKey(input.toUpperCase())){
+						if (inventory.getProductInfoBySlotMap().containsKey(input.toUpperCase())) {
+							System.out.println("This bit still works");
+							//System.out.println(inventory.getTesterMap().get());
+						} else
+							System.out.println("This item does not exist. Please try again.");
 
-					}else if (purchasingDecision.equals(PURCHASE_MENU_FINISH_TRANSACTION)){
+					} else if (purchasingDecision.equals(PURCHASE_MENU_FINISH_TRANSACTION)) {
 
 					}
-					System.out.println("Current Money Provided: \n"+"$"+cashBank.getMoneyProvided());
+					System.out.println("Current Money Provided: \n" + "$" + cashBank.getMoneyProvided());
 				}
-			} else if (choice.equals(MAIN_MENU_OPTION_EXIT)){
+			} else if (choice.equals(MAIN_MENU_OPTION_EXIT)) {
 				System.out.println("Thank you, come again!");
 				System.exit(0);
 			}
@@ -100,13 +100,5 @@ public class VendingMachineCLI {
 		VendingMachineCLI cli = new VendingMachineCLI(menu);
 		cli.run();
 	}
-
-	public static void displayMenuOptions(){
-		for (String[] item : inventory.getInventoryList()){
-			for(String detail : item){
-				System.out.print(detail+" ");
-			}
-			System.out.println();
-		}
-	}
 }
+
