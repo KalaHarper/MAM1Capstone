c1321ad533a7cdd302d6a0eb7da3ac42eaae0dc1
Kala Harper <kala.n.harper@gmail.com>
2022-04-13 18:04:13 -0500

    Fixed the date and time

--- capstone/src/main/java/com/techelevator/Ledger.java
+++ capstone/src/main/java/com/techelevator/Ledger.java
@@ -2,9 +2,11 @@ package com.techelevator;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.Calendar;
 
 
 public class Ledger {
@@ -12,11 +14,9 @@ public class Ledger {
 
     public void printsToLog(String info){
         try( PrintWriter writer = new PrintWriter("capstone\\log.txt")){
-           LocalDate date = LocalDate.now();
-           LocalTime time = LocalTime.now();
-            DateTimeFormatter dateTimeFormatted = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-            writer.append(date + " " + time + " " + info);
-            writer.flush();
+            Calendar calendar = Calendar.getInstance();
+            SimpleDateFormat simpleDate = new SimpleDateFormat("MM/dd/yyyy hh:mm:s");
+            writer.println(simpleDate.format(calendar.getTime()) + " " + info);
         }   catch(FileNotFoundException e){
                 System.err.println("No log text file loaded.");
                 System.exit(1);
