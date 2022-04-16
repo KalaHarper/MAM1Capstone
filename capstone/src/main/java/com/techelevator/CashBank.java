b009ad469dd058135655a493ecdf63cd9abbbf92
Kala Harper <kala.n.harper@gmail.com>
2022-04-16 18:35:27 -0500

    made some tests

--- capstone/src/main/java/com/techelevator/CashBank.java
+++ capstone/src/main/java/com/techelevator/CashBank.java
@@ -1,8 +1,6 @@
 package com.techelevator;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
-import java.text.NumberFormat;
 
 public class CashBank {
     private static final BigDecimal ZERO = new BigDecimal("0.00");
@@ -43,14 +41,9 @@ public class CashBank {
         System.out.println("Have a nice day!");
     }
 
-    public BigDecimal getReturnAmount(){
+    public static BigDecimal getReturnAmount(){
         moneyProvided = moneyProvided.subtract(cost);
         cost = BigDecimal.valueOf(0.00);
-
-        //BigDecimal modifier = new BigDecimal(moneyProvided - cost);
-       // MathContext thismayaswellhappen = new MathContext(2);
-       // double change = modifier.doubleValue();
-        //return NumberFormat.getCurrencyInstance().format(modifier);
         return moneyProvided;
 
     }
--- capstone/src/test/java/com/techelevator/CashBankTest.java
+++ capstone/src/test/java/com/techelevator/CashBankTest.java
@@ -1,17 +1,63 @@
 package com.techelevator;
 
 import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.math.BigDecimal;
 
 public class CashBankTest extends TestCase {
-
+    @Test
     public void testAddMoney() {
-
+        //Arrange
+        BigDecimal expected = new BigDecimal("3.30");
+        BigDecimal jorjay = new BigDecimal("1.65");
+        //Act
+        CashBank.addMoney(jorjay);
+        CashBank.addMoney(jorjay);
+        //Assert
+        Assert.assertEquals(expected, CashBank.getMoneyProvided());
     }
 
-    public void testMakeChange() {
+    @Test
+    public void testMakeChange(){
+        //Expected amount format
+        BigDecimal piccolo = new BigDecimal("0.00");
+        //Amounts added to moneyProvided
+        BigDecimal vegeta = new BigDecimal("0.25");
+        BigDecimal goku = new BigDecimal("1.85");
+
+        //added the money
+        CashBank.addMoney(goku);
+        CashBank.addMoney(vegeta);
+
+        //calling method in question
+        CashBank.makeChange();
+
+        //Assert
+        Assert.assertEquals(piccolo, CashBank.getMoneyProvided());
     }
 
     public void testGetReturnAmount() {
+        //Expected
+        BigDecimal courageTheCowardlyDog = new BigDecimal("0.00");
+
+        //Adding this much
+        BigDecimal muriel = new BigDecimal("3.00");
+
+        //Subtracting this much
+        BigDecimal eustace = new BigDecimal("1.50");
+
+        //Calling methods
+        CashBank.addMoney(muriel);
+        CashBank.getReturnAmount();
+
+        Assert.assertEquals(courageTheCowardlyDog, CashBank.getMoneyProvided());
+
+
     }
 }
